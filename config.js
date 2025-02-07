a5caf670ef90e5c28da1247dbac8364ea61562dc
SCRIPTNZA <80497094+script301@users.noreply.github.com>
2025-02-07 01:16:26 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    modoOffline: true, // Conexão no modo offline (true) ou online (false)
+    carregarChunks: true, // Movimentação para carregar chunks
+    atacarMobs: true, // Atacar mobs hostis automaticamente
+    dormirEmCama: true, // Dormir em camas próximas
+    comerComida: true, // Comer qualquer comida disponível(exceto carne podre)
+    quebrarBlocos: false, // Ativar quebra de blocos
+    mensagensAutomaticas: true, // Enviar mensagens no chat a cada 10 minutos
+};
