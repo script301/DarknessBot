3fc430d85556b17c8783a144d2c7ef29cb597aa4
SCRIPTNZA <80497094+script301@users.noreply.github.com>
2025-02-08 14:56:46 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
-const configPath = './database.json'; // Caminho para o arquivo de dados
+const configPath = './database.json';
 
-// Função para carregar o banco de dados
 const carregarDados = () => {
     if (fs.existsSync(configPath)) {
         const data = fs.readFileSync(configPath);
         return JSON.parse(data);
     } else {
-        return null; // Caso o arquivo não exista, retorna null
+        return null;
     }
 };
 
-// Função para salvar dados no banco de dados
 const salvarDados = (dados) => {
     fs.writeFileSync(configPath, JSON.stringify(dados, null, 2), 'utf8');
 };
